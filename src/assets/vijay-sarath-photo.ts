9025a285f4574c71974da5cbf4f303e7425ef9e2
CodeDilli <g.dilliganesh99@gmail.com>
2026-09-14 14:32:20 +0530

    Photo 3-part placeholders

--- src/assets/vijay-sarath-photo.ts
+++ src/assets/vijay-sarath-photo.ts
@@ -1,3 +1,4 @@
-const _p1 = "P1_PLACEHOLDER";
-const _p2 = "P2_PLACEHOLDER";
-export const vijaySarathPhotoDataUrl = "data:image/jpeg;base64," + _p1 + _p2;
+const _p0 = "PART0";
+const _p1 = "PART1";
+const _p2 = "PART2";
+export const vijaySarathPhotoDataUrl = "data:image/jpeg;base64," + _p0 + _p1 + _p2;
